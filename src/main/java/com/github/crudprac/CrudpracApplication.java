2f6bd24df55ed5ca7c9342292bc6863458e03c40
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-08-07 18:22:15 +0900

    no message

---
+++ src/main/java/com/github/crudprac/CrudpracApplication.java
@@ -0,0 +1,13 @@
+package com.github.crudprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudpracApplication.class, args);
+    }
+
+}
